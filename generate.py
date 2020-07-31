9d88737bc7c9be348c05eb1cb46b17ec1329997d
Sepehr Sameni <Sepehr.Sameni@gmail.com>
2020-07-31 19:15:02 +0430

    done for now

--- generate.py
+++ generate.py
@@ -97,7 +97,7 @@ def generate_fast_attention_table():
         '|Paper (citations)|Code|Complexity|AutoRegressive|Main Idea|',
         '|:---:|:---:|:---:|:---:|:---:|']
     generated_lines = []
-    meta_info = get_and_sort_meta_info('FastAttention_full.json')
+    meta_info = get_and_sort_meta_info('FastAttention.json')
     for item in tqdm(meta_info):
         citation, date, paper, abstract = fetch_common_parts(item)
         if 'code' in item:
